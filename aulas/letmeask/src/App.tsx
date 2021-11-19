c13ea5b89e14579be7983bce52a87bfcecef0d22
wendelsilva <wendel245.ws@gmail.com>
2021-11-18 22:33:22 -0300

    feat(auth): created auth context and hooks and resolve function error

--- aulas/letmeask/src/App.tsx
+++ aulas/letmeask/src/App.tsx
@@ -1,54 +1,19 @@
-import { createContext, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
-import { auth, firebase } from "./services/firebase";
-
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}
-
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-}
-
-export const AuthContext = createContext({} as AuthContextType);
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-  const [user, setUser] = useState<User>();
-
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider);
-
-    if(result.user) {
-      const { displayName, photoURL, uid } = result.user;
-
-      if(!displayName || !photoURL) {
-        throw new Error("User is missing displayName or photoURL");
-      }
-
-      setUser({
-        id: uid,
-        name: displayName, 
-        avatar: photoURL
-      });
-    }
-  }
-
   return (
     <BrowserRouter>
-        <AuthContext.Provider value={{ user, signInWithGoogle }}/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rooms/new" element={<NewRoom />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+        </Routes>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- aulas/letmeask/src/pages/NewRoom.tsx
+++ aulas/letmeask/src/pages/NewRoom.tsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -6,13 +5,13 @@ import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 
 export function NewRoom() {
-    const { user } = useContext(AuthContext);
+    const { user } = useAuth();
 
     return (
         <div id="page-auth">
@@ -24,7 +23,6 @@ export function NewRoom() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <h1>{user?.name}</h1>
                     <h2>Criar uma nova sala</h2>
                     <form>
                         <input 
